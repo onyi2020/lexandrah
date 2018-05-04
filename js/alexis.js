680cf610d601e6478341b61e0cc7012aed845807
onyi2020 <mbamaraonyinye@gmail.com>
2018-05-04 05:32:17 +0100

    first commit

---
+++ js/alexis.js
@@ -0,0 +1,24 @@
+/**this function initialises the required variables and makes an API call**/
+var lives ="you have 3 lives left";
+	let name = "name";
+$(document).ready(function(){
+	var prompt = "please, " + name + lives
+	return name + lives;
+});
+
+
+
+/**$(document).on('ready',function() {
+	$('#mona-connect').click(function(e){
+		e.preventDefault();
+		var reference_id = 'MG1524413555';
+		var merchant_id = "999";
+		var product_key = 'b2662a1858df3d0c5ae502195f840be7be39079b';
+		var uuid = '43456796gvbndcvbnmnbv';
+		var amount = 2300;
+		var description = 'Buy+MaliyoGame+Token5000+for+50+NGN';
+		window.open('https://www.monapay.com/v1/merchant/pay?reference_id=' + reference_id 
+			+ '&merchant_id=' + merchant_id + '&key=' + product_key + '&uuid=' + uuid + '&amount=' + amount 
+			+ '&description=' + description);
+	});
+})***/
